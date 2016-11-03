7e4fe399b43e6cbb22feed784313a136b44752c5
马勒 <wusanpang@qq.com>
2016-11-03 18:27:04 +0800

    Update ReducerOfIPFilter.java

--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -7,13 +7,10 @@ public class ReducerOfIPFilter {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 		
-		int keycount = 0;
 		String key = null;
 		HashSet<String> setKey = new HashSet<String>();
 		while (scanner.hasNext()){
-			String line = scanner.nextLine();
-			String[] tokens = line.split("\t");
-			key = tokens[0];
+			key = scanner.next();
 			setKey.add(key);
 		}
 		System.out.println(setKey.size());
