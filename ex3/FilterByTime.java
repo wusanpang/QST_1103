72470471a60e66b2da8e9f378ce4b2bf49851b35
马勒 <wusanpang@qq.com>
2016-11-03 16:07:15 +0800

    Create FilterByTime.java

--- ex3/FilterByTime.java
+++ ex3/FilterByTime.java
@@ -5,7 +5,10 @@ import java.io.FileNotFoundException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * 题目要求：
@@ -18,20 +21,40 @@ import java.util.Scanner;
 public class FilterByTime {
 	
 	public static void main(String[] args) throws ParseException, FileNotFoundException{
+		
+		Locale locale = Locale.US; 
+		SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", locale);
+		
 		SimpleDateFormat regularFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date beginDate = regularFormat.parse("2015-12-31 18:00:00");
 		Date endDate = regularFormat.parse("2015-12-31 19:00:00");
-		String filePath = "./access.log";
+		
+		String filePath = "H:/Tmp./access.log";
 		FileInputStream inputStream = new FileInputStream(filePath);
 		Scanner scanner = new Scanner(inputStream, "UTF-8");
+		
+		String pattern = "(\\d+.\\d+.\\d+.\\d+) [^ ]* [^ ]* \\[([^ ]* [^ ]*)\\] \"[^ ]+ ([^ ]+) .*\" \\d+ \\d+ \"(.*)\" \"(.*)\"";
+		Pattern p = Pattern.compile(pattern);
+		
 		while (scanner.hasNext()){
 			// 对每行进行处理
 			String line = scanner.nextLine();
 			// 切分获取IP，Time
 			String strIp = null;
 			String strTime = null;
-			// 对在时间区间内的数据进行输出
-			System.out.println(strIp + "\t" + strTime);
+			
+
+			Matcher m = p.matcher(line);
+			if (m.find()) {
+				strIp = m.group(1);
+				strTime = m.group(2);
+				Date Time = inputFormat.parse(strTime);
+				if (Time.before(endDate) && Time.after(beginDate)) {
+					
+					// 对在时间区间内的数据进行输出
+					System.out.println(strIp + "\t" + regularFormat.format(Time));
+				}
+			}
 		}
 	}
 	
